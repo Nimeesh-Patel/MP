c85a9bb2b7e31e99badea5837035b6c6551525fc
Nimeesh Patel <nimeeshpatel.np@gmail.com>
2025-07-11 19:42:04 +0530

    Remove BEM comment

--- src/App.js
+++ src/App.js
@@ -3,23 +3,13 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import "./App.css";
-import Login from "./Login";
-import Signup from "./Signup";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    // BEM
     <div className="app">
       <Sidebar />
       <Feed />
       <Widgets />
-      {/* <Router>
-        <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </Switch>
-      </Router> */}
     </div>
   );
 }
