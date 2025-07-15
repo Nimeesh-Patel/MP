1f0b57524d0b057cca58024e3d1d87d8d896638f
AbhinavNair0077 <abhinavnair0077@gmail.com>
2025-07-16 00:28:26 +0530

    Revert " adding practice section"
    This reverts commit ac434af5f130af757b1262e3f4a3affef92c091c.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import "./App.css";
-import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
-import Practice from "./Practice";
-import { BrowserRouter as Router, Route, Routes, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
@@ -15,7 +13,6 @@ function App() {
     <div className="app">
       <Sidebar />
       <Feed />
-
       <Widgets />
       {/* <Router>
         <Switch>
@@ -23,20 +20,6 @@ function App() {
           <Route path="/signup" component={Signup} />
         </Switch>
       </Router> */}
-
-      {/* <Widgets />  */}
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/practice" component={Practice} />
-      </Switch>
-
-        {/* <Switch>
-          <Route exact path="/" component={Login} />
-          <Route path="/signup" component={Signup} />
-        </Switch> */}
-      </Router>
-
     </div>
   );
 }
